2bc42aa9a873cc5247eca709e3b43468568df88c
Adrianna Foster <adrifost@ucar.edu>
2021-12-27 19:40:47 -0700

    update parameter modify script

--- modify_param.py
+++ modify_param.py
@@ -1,89 +1,97 @@
-from datetime import date
+"""
+Modifies FATES parameter file based on an input csv file.
+"""
 import os
 import pandas as pd
 
-def modify_fates_params(file_work, tag, src_dir, param_file):
-    
-    # File name of final nc file
-    today = str(date.today())
-    mod_fname = file_work + '/param_file_' + tag + '.nc'
 
-    # Create a string of the pfts we want
-    pfts = param_file.pft_index.values
-    pft_list = ''
-    for pft in pfts:
-        pft_list = pft_list + str(pft) + ','
+def nc_generate(out_dir, src_dir, pfts, out_file):
+    """
+    Generates a FATES default parameter file with only the pfts specified
+    """
+    # name of output file with swapped PFTs
+    temp_file = os.path.join(out_dir, "param_file_temp0.nc")
 
-    # We don't want final comma
-    pft_list = pft_list[:len(pft_list)-1]
-
-    # Get default parameters for desired pfts
-    nc_generate(file_work, tag, src_dir, pft_list, mod_fname)
-
-    # Change pft names
-    rename_pfts(src_dir, mod_fname, param_file)
-
-    # Variables to modify
-    vars = param_file.columns[2:].values
-
-    # Modify all other params
-    for i in range(len(vars)):
-        vals = param_file[vars[i]].values
-        for j in range(len(vals)):
-            change_value(src_dir, mod_fname, str(vars[i]), str(vals[j]), str(j + 1))
-
-
-    command = src_dir + '/tools/modify_fates_paramfile.py --fin ' + mod_fname + ' --fout ' + mod_fname + ' --O --var fates_mort_disturb_frac --val 0.5'
-    os.system(command)
-
-    command = 'rm ' + file_work + '/param_file_' + tag + '_temp0.nc'
-    os.system(command)
-
-def change_value(src_dir, mod_fname, var, val, pft):
-
-    script = src_dir + '/tools/modify_fates_paramfile.py'
-    files = ' --fin ' + mod_fname + ' --fout ' + mod_fname + ' --O '
-
-    command = script + files + ' --PFT ' + pft + ' --var ' + var + ' --val ' + val
-
-    os.system(command)
-
-
-def nc_generate(file_work, tag, src_dir, pft_list, mod_fname):
-
-
-    init_file = file_work + '/param_file_' + tag + '_temp0.nc'
-    default_file = src_dir + '/parameter_files/fates_params_default.cdl'
+    # name of default input file
+    default_file = os.path.join(src_dir, "parameter_files/fates_params_default.cdl")
 
     # generate netcdf binary from the cdl file
-    command = 'ncgen -o ' + init_file + ' ' + default_file
-    os.system(command)
- 
-    # pull out pfts of interest (needle leaf evergreen: 2; broadleaf decid tree: 6)
-    command = src_dir + '/tools/FatesPFTIndexSwapper.py --fin ' + init_file + ' --fout ' + mod_fname + \
-              " --pft-indices " + pft_list
-    os.system(command)
-    
-def rename_pfts(src_dir, mod_fname, param_file):
+    os.system(f"ncgen -o {temp_file} {default_file}")
 
+    # list of pfts from array
+    pft_list = str(pfts).join(",")
+
+    # pull out pfts of interest
+    script = os.path.join(src_dir, 'tools/FatesPFTIndexSwapper.py')
+    command = f"{script} --fin {temp_file} --fout {out_file} --pft-indices {pft_list} "
+    os.system(command)
+
+
+def rename_pfts(src_dir, file_name, param_file):
+    """
+    Renames PFTs for a FATES parameter file based on an input file.
+    """
+    # pull out pft names from input file
     pft_names = param_file.pft_name.values
 
-    for i in range(len(pft_names)):
-        command = src_dir + '/tools/modify_fates_paramfile.py --fin ' + mod_fname + ' --fout ' + mod_fname + \
-            ' --O --var fates_pftname --val ' + str(pft_names[i]) + ' --PFT ' + str(i + 1)
-        
+    # rename the pfts
+    script = os.path.join(src_dir, "tools/modify_fates_paramfile.py")
+    for num, pft in enumerate(pft_names):
+        command = f"{script} --fin {file_name} --fout {file_name} --O --var fates_pftname " \
+                  f"--val {pft} --PFT {num + 1}"
         os.system(command)
+
+def change_value(src_dir, out_file, var, val, pft):
+    """
+    Changes the values of a FATES parameter file for a specified parameter
+    """
+
+    script = os.path.join(src_dir, "/tools/modify_fates_paramfile.py")
+    files = f" --fin {out_file} --fout {out_file} --0 "
+    command = f"{script} {files} --PFT {pft} --var {var} --val {val}"
+    os.system(command)
+
+def modify_fates_params(out_dir, tag, src_dir, param_file):
+    """
+    Modifies a FATES parameter file based on an input data frame
+    """
+    # File name of final nc file
+    out_file = os.path.join(out_dir, f"param_file_{tag}.nc")
+
+    # create a string of the pfts we want
+    pfts = param_file.pft_index.values
+
+    # get default parameters for desired pfts
+    nc_generate(out_dir, src_dir, pfts, out_file)
+
+    # change pft names
+    rename_pfts(src_dir, out_file, param_file)
+
+    # parmaeters to modify
+    params = param_file.columns[2:].values
+
+    # Modify all other params
+    for parm in enumerate(params):
+        vals = param_file[parm].values
+        for vnum, val in vals:
+            change_value(src_dir, out_file, str(parm), str(val), str(vnum + 1))
+
+    # set disturbance fraction to 0.5
+    script = os.path.join(src_dir, "tools/modify_fates_paramfile.py")
+    command = f"{script} --fin {out_file} --fout {out_file} --O --var fates_mort_disturb_frac " \
+              f"--val 0.5 "
+    os.system(command)
+
+    # delete the temporary file
+    os.system(f"rm {os.path.join(out_dir, 'param_file_temp0.nc')}")
 
 
 if __name__ == "__main__":
-    src_dir = '/project/tss/afoster/ctsm_fates/src/fates'
-    file_work = '/project/tss/afoster/FATES_params'
-    tag = 'canada'
-    infile = '/project/tss/afoster/FATES_params/fates_yukon_parameters.csv'
-    
+    CODE_DIR = "/project/tss/afoster/ctsm_fates/src/fates"
+    OUTPUT_DIR = "/project/tss/afoster/FATES_params"
+    FILE_TAG = "canada"
+    INFILE = "/project/tss/afoster/FATES_params/fates_yukon_parameters.csv"
+
     # Generate the new parameter file
-    param_file = pd.read_csv(infile)
-    modify_fates_params(file_work, tag, src_dir, param_file)
-
-
-
+    param_dat = pd.read_csv(INFILE)
+    modify_fates_params(OUTPUT_DIR, FILE_TAG, CODE_DIR, param_dat)
